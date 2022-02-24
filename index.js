f2b7a32792763f8ce2e03f2523011fc586ddbd03
Kane Mathis <admin@kanemathis.com>
2022-02-24 13:26:27 -0700

    Kane's Update

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+const message = [];
+
+function writeCards(name, event){
+for (i=0; i<name.length; i++){
+    
+message.push(`hello ${name[i]} join us at ${event}`); 
+
+}
+console.log(message);
+}
+
+writeCards (["carla", "jim", "dale"], "party time");
+
+//countdown
+
+function countDown (number){
+    while( number>0) {number--
+        console.log(number);
+    }
+}
+
+countDown(17);
